cb60f36c6f08520e53eceafa5939a7235bc656ed
OtaRyo <nwtgck@gmail.com>
2016-08-10 07:28:57 +0900

    [experiment] RSフリップフロップの出力の挙動を通常通りにしようとしていろいろ試した（うまくはいってない）

--- wire.hs
+++ wire.hs
@@ -290,6 +290,34 @@ rsff = proc (s, r) -> do
     q_    <- nandGate -< (r_, preQ)
   returnA -< (q, q_)
 
+-- RS flip-flop
+-- TODO 最初の出力が(I, I)になってしまう（最初だけなので、大量に流すときは些細なことになると思うが）
+-- TODO 前がresetの時にsetにすると(I, O)となってほしいが(I, I)となる。ハザードなのかもしれない
+-- おそらく同じ信号をある程度（2連続ぐらい）流し続ける必要がありそう
+rsff2 :: SF (Bit, Bit) (Bit, Bit)
+rsff2 = proc (s, r) -> do
+  s_    <- invGate -< s
+  r_    <- invGate -< r
+  rec
+    preQ  <- dHold O -< Event q      -- init q  is X, and holds calculated value q
+    preQ_ <- dHold O -< Event q_     -- init q_ is X, and holds calculated value q_
+    q     <- nandGate -< (s_, preQ_)
+    q_    <- nandGate -< (r_, preQ)
+  returnA -< (q, q_)
+
+-- RS flip-flop
+-- TODO 最初の出力が(I, I)になってしまう（最初だけなので、大量に流すときは些細なことになると思うが）
+-- TODO 前がresetの時にsetにすると(I, O)となってほしいが(I, I)となる。ハザードなのかもしれない
+-- おそらく同じ信号をある程度（2連続ぐらい）流し続ける必要がありそう
+norRsff :: SF (Bit, Bit) (Bit, Bit)
+norRsff = proc (s, r) -> do
+  rec
+    preQ  <- dHold O -< Event q      -- init q  is X, and holds calculated value q
+    preQ_ <- dHold O -< Event q_     -- init q_ is X, and holds calculated value q_
+    q     <- norGate -< (r, preQ_)
+    q_    <- norGate -< (s, preQ)
+  returnA -< (q, q_)
+
 -- -- RS Flip-Flop -- NOR Base
 -- rsff :: SF (Bit, Bit) (Bit, Bit, (Bit, Bit))
 -- rsff = proc (s, r) -> do
@@ -452,8 +480,9 @@ testForRsff = do
   --     tabu  = (I, I)
 
   print $ embed
-    (rsff) -- 使いたいSF
-    (reset, [(0.1, Just e) | e <- [reset, hold, set, set]])--, set, hold, set, reset, reset, hold, hold, hold] ])
+    (norRsff) -- 使いたいSF
+    -- (reset, [(0.1, Just e) | e <- [reset, hold, set, set]])--, set, hold, set, reset, reset, hold, hold, hold] ])
+    (hold, [(0.1, Just e) | e <- [hold, set, set, reset, reset, reset, reset, hold, hold, set, set, hold, hold]])--, set, hold, set, reset, reset, hold, hold, hold] ])
 
 orTest = do
   print $ I #| undefined -- I
@@ -461,5 +490,6 @@ orTest = do
 
 main :: IO ()
 main = do
-  print $ fillBits O n5
-  print $ unknowns n6
+  -- print $ fillBits O n5
+  -- print $ unknowns n6
+  testForRsff
