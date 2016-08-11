a755e796ca704f38daa166292db37c7ba471877f
OtaRyo <nwtgck@gmail.com>
2016-08-11 18:55:16 +0900

    [add] 加算器のための自動作成を作った

--- auto-creator/auto-creator.hs
+++ auto-creator/auto-creator.hs
@@ -19,4 +19,23 @@ main = do
       forM_ [1..n] $ \i -> printf "N%d," i
       putStrLn ""
 
+      putStr "["
+      forM_ [n-1, n-2..0] $ \i -> do
+        printf "a%d," i
+      putStrLn "]"
+
+      putStr "["
+      forM_ [n-1,n-2..0] $ \i -> do
+        printf "b%d," i
+      putStrLn "]"
+
+      forM_ [1..n-1] $ \i -> do
+        printf "(c%d, s%d) <- fullAdder -< (a%d, b%d, c%d)\n" i i i i (i-1)
+      putStrLn ""
+
+      forM_ [n-1, n-2..0] $ \i -> printf "s%d:*" i
+      putStrLn "End"
+      -- s3:*s2:*s1:*s0:*End
+
+
     _ -> putStrLn ("input error\nusage: auto-creator 32")
