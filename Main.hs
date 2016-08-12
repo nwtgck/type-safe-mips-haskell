dbc5b14e1d0f6518a7756ecc15ad0631b935e4d9
OtaRyo <nwtgck@gmail.com>
2016-08-13 07:54:16 +0900

    [refactor] 動作テストをしていたものをMainから分けた

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
--- Yampaを使って論理回路を作る
+-- Yampaを使ってMIPSアーキテクチャを作る
 
 {-# LANGUAGE Arrows        #-}
 {-# LANGUAGE GADTs         #-}
@@ -25,78 +25,6 @@ memTest = proc (input, writeFlag) -> do
   output <- dHold (fillBits O n4) -< if writeFlag == I then Event input else NoEvent
   returnA -<  output
 
--- Test for 4 bits adder
-testForAdd4bits = do
-  prevOut <- newIORef (undefined :: Bits N4)
-  let zero = O:*O:*O:*O:*End
-      one  = O:*O:*O:*I:*End
-  reactimate (return (zero, zero))
-               (\_ -> do
-                 threadDelay 100000
-                 out <- readIORef prevOut
-                 return (0.1, (Just $ (out, one) )))
-               (\_ out -> print (out, bitsToIntMaybe out) >> writeIORef prevOut out >> return False)
-               (add4Bits)
-
--- Test for 32 bits adder
-testForAdd32bits = do
- prevOut <- newIORef (undefined :: Bits N32)
- let zero = fillBits O n32
-     one  = O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*I:*End
- reactimate (return (zero, zero))
-              (\_ -> do
-                -- threadDelay 80000
-                out <- readIORef prevOut
-                return (0.1, (Just $ (out, one) )))
-              (\_ out -> print (out, bitsToIntMaybe out) >> writeIORef prevOut out >> return False)
-              (add32Bits)
-
--- Test for 32 bits sub
-testForSub32bits = do
- prevOut <- newIORef (undefined :: Bits N32)
- let
-     zero = fillBits O n32
-     maxB = fillBits I n32
-     one  = O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*I:*End
- reactimate (return (maxB, zero))
-              (\_ -> do
-                -- threadDelay 80000
-                out <- readIORef prevOut
-                return (0.1, (Just $ (out, one) )))
-              (\_ out -> print (out, bitsToIntMaybe out) >> writeIORef prevOut out >> return False)
-              (sub32Bits)
-
--- Test for 4 bits sub
-testForSub4bits = do
- prevOut <- newIORef (undefined :: Bits N4)
- let
-     zero = O:*O:*O:*O:*End
-     b15 = I:*I:*I:*I:*End
-     one  = O:*O:*O:*I:*End
-     a = embed (sub4Bits) ((b15, zero), [])
- reactimate (return (b15, zero))
-              (\_ -> do
-                threadDelay 100000
-                out <- readIORef prevOut
-                return (0.1, (Just $ (out, one) )))
-              (\_ out -> print (out, bitsToIntMaybe out) >> writeIORef prevOut out >> return False)
-              (sub4Bits)
-
--- Test for 4 bits and
-testForAnd4bits = do
- prevOut <- newIORef (undefined :: Bits N4)
- let
-     zero = O:*O:*O:*O:*End
-     b15 = I:*I:*I:*I:*End
-     one  = O:*O:*O:*I:*End
-     a = embed (sub4Bits) ((b15, zero), [])
- reactimate (return (b15, zero))
-              (\_ -> do
-                threadDelay 100000
-                out <- readIORef prevOut
-                return (0.1, (Just $ (out, one) )))
-              (\_ out -> print (out, bitsToIntMaybe out) >> writeIORef prevOut out >> return False)
-              (and4Bits)
 
 -- Test for 4 bits adder and Memory
 testForAdd4bitsAndMem = do
---
+++ Test.hs
@@ -0,0 +1,135 @@
+{-# LANGUAGE Arrows        #-}
+{-# LANGUAGE GADTs         #-}
+{-# LANGUAGE TypeFamilies  #-}
+{-# LANGUAGE TypeOperators #-}
+
+
+import           BasicUnit
+import           Bit
+import           Control.Concurrent
+import           Data.IORef
+import           Data.Maybe
+import           Debug.Trace
+import           DebugSF
+import           DelayedSF
+import           FRP.Yampa
+import           Natural
+import           Text.Printf
+import           Unsafe.Coerce
+
+
+-- Test for 4 bits adder
+testForAdd4bits = do
+  prevOut <- newIORef (undefined :: Bits N4)
+  let zero = O:*O:*O:*O:*End
+      one  = O:*O:*O:*I:*End
+  reactimate (return (zero, zero))
+               (\_ -> do
+                 threadDelay 100000
+                 out <- readIORef prevOut
+                 return (0.1, (Just $ (out, one) )))
+               (\_ out -> print (out, bitsToIntMaybe out) >> writeIORef prevOut out >> return False)
+               (add4Bits)
+
+-- Test for 32 bits adder
+testForAdd32bits = do
+ prevOut <- newIORef (undefined :: Bits N32)
+ let zero = fillBits O n32
+     one  = O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*I:*End
+ reactimate (return (zero, zero))
+              (\_ -> do
+                -- threadDelay 80000
+                out <- readIORef prevOut
+                return (0.1, (Just $ (out, one) )))
+              (\_ out -> print (out, bitsToIntMaybe out) >> writeIORef prevOut out >> return False)
+              (add32Bits)
+
+-- Test for 32 bits sub
+testForSub32bits = do
+ prevOut <- newIORef (undefined :: Bits N32)
+ let
+     zero = fillBits O n32
+     maxB = fillBits I n32
+     one  = O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*O:*I:*End
+ reactimate (return (maxB, zero))
+              (\_ -> do
+                -- threadDelay 80000
+                out <- readIORef prevOut
+                return (0.1, (Just $ (out, one) )))
+              (\_ out -> print (out, bitsToIntMaybe out) >> writeIORef prevOut out >> return False)
+              (sub32Bits)
+
+-- Test for 4 bits sub
+testForSub4bits = do
+ prevOut <- newIORef (undefined :: Bits N4)
+ let
+     zero = O:*O:*O:*O:*End
+     b15 = I:*I:*I:*I:*End
+     one  = O:*O:*O:*I:*End
+     a = embed (sub4Bits) ((b15, zero), [])
+ reactimate (return (b15, zero))
+              (\_ -> do
+                threadDelay 100000
+                out <- readIORef prevOut
+                return (0.1, (Just $ (out, one) )))
+              (\_ out -> print (out, bitsToIntMaybe out) >> writeIORef prevOut out >> return False)
+              (sub4Bits)
+
+-- Test for 4 bits and
+testForAnd4bits = do
+ prevOut <- newIORef (undefined :: Bits N4)
+ let
+     zero = O:*O:*O:*O:*End
+     b15 = I:*I:*I:*I:*End
+     one  = O:*O:*O:*I:*End
+     a = embed (sub4Bits) ((b15, zero), [])
+ reactimate (return (b15, zero))
+              (\_ -> do
+                threadDelay 100000
+                out <- readIORef prevOut
+                return (0.1, (Just $ (out, one) )))
+              (\_ out -> print (out, bitsToIntMaybe out) >> writeIORef prevOut out >> return False)
+              (and4Bits)
+
+
+-- メモリとしての機能を果たすか作ってみて確かめる
+memTest :: SF (Bits N4, Bit) (Bits N4)
+memTest = proc (input, writeFlag) -> do
+  output <- dHold (fillBits O n4) -< if writeFlag == I then Event input else NoEvent
+  returnA -<  output
+
+
+-- Test for 4 bits adder and Memory
+testForAdd4bitsAndMem = do
+ let zero = O:*O:*O:*O:*End
+     one  = O:*O:*O:*I:*End
+     mainSF :: SF Bit (Bits N4)
+     mainSF = proc writeFlag -> do
+      rec
+        memData <- memTest  -< (added, writeFlag)
+        added   <- add4Bits -< (memData, one)
+      returnA -< added
+
+ reactimate (return O)
+            (\_ -> threadDelay 100000 >> return (0.1, Just I))
+            (\_ out -> print (out, bitsToIntMaybe out) >> return False)
+            (mainSF)
+
+-- Test for delayed 4 bits adder and Memory
+testForDelayAdd4bitsAndMem = do
+ let zero = O:*O:*O:*O:*End
+     one  = O:*O:*O:*I:*End
+     mainSF :: SF Bit (Bits N4)
+     mainSF = proc writeFlag -> do
+      rec
+        memData  <- memTest  -< (out, writeFlag)
+        out    <- delayedSF 0.5 (unknowns n4) add4Bits -< (memData, one)
+      returnA -< out
+
+ reactimate (return O)
+            (\_ -> threadDelay 100000 >> return (0.1, Just I))
+            (\_ out -> print (out, bitsToIntMaybe out) >> return False)
+            (mainSF)
+
+
+main = testForAdd32bits
